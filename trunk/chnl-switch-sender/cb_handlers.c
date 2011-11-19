c0a5ca84b9e8dcc9864942c75813055429aeba42
harasimczuk@gmail.com <harasimczuk@gmail.com@242b7e36-f81d-2809-4fc0-0586400e26fa>
2011-11-19 11:42:03 +0000

    Wysyłanie Probe Request z IEs

---
+++ trunk/chnl-switch-sender/cb_handlers.c
@@ -0,0 +1,200 @@
+/*
+ * Author: Marcin Harasimczuk
+ *
+ * Callback handlers used while recieving messages. 
+ * err, fin and ack handler control the flow of recieving.
+ * my_cb_handler is for parsing and printing data from messages
+ * to the standard output.
+ */
+
+/* called on error */
+static int err_handler(struct sockaddr_nl *nla, struct nlmsgerr *err,
+			 void *arg)
+{
+	printf("err=%d\n", err->error);
+	int *ret = arg;
+	*ret = 0;
+	return NL_STOP;
+}
+/* called for last packet in series of packets */
+static int fin_handler(struct nl_msg *msg, void *arg)
+{
+	//printf("fin\n");
+	int *ret = arg;
+	*ret = 0;
+	return NL_SKIP;
+}
+/* called when ack is needed */
+static int ack_handler(struct nl_msg *msg, void *arg)
+{
+	printf("ack\n");
+	int *ret = arg;
+	*ret = 0;
+	return NL_STOP;
+}
+/* Custom callback handler for printing data form valid message */
+static int my_cb_handler(struct nl_msg *msg, void *arg)
+{
+	/* Attribute pointers (loop counters) for expected nested attributes */
+	struct nlattr *pbnd;		// band attribute
+	struct nlattr *pfrq;		// frequency attribute
+	struct nlattr *pbtr;		// bitrate attribute
+	struct nlattr *pmod;		// mode attribute
+	struct nlattr *pcmd;		// commands attribute
+	
+	/* Length of nested attributes stream (remaining) for loop */
+	int rem_bnd; 
+	int rem_frq;
+	int rem_btr;
+	int rem_mod;
+	int rem_cmd;
+	
+	/* Attribute buffer for message */
+	struct nlattr *msg_attr_buff[NL80211_ATTR_MAX + 1];
+	/* Attribute buffer for attribute band nested attributes */
+	struct nlattr *bnd_attr_buff[NL80211_BAND_ATTR_MAX + 1];
+	/* Attribute buffer for attribute frequency nested attributes */
+	struct nlattr *frq_attr_buff[NL80211_FREQUENCY_ATTR_MAX + 1];
+	/* Attribute buffer for attribute bitrate nested attributes */
+	struct nlattr *btr_attr_buff[NL80211_BITRATE_ATTR_MAX + 1];
+	
+	/* band identifier */
+	int id_bnd = 0;
+	
+	/*
+	 * Attribute policy (validation for parsing) 
+	 *
+	 * NL80211_FREQUENCY_ATTR_FREQ : frequency in MHz [32 bit integer] 
+	 * NL80211_FREQUENCY_ATTR_DISABLED : channel disabled in current domain [bool]
+	 * NL80211_FREQUENCY_ATTR_PASSIVE_SCAN : only passive scan on channel [bool]
+	 * NL80211_FREQUENCY_ATTR_NO_IBSS : IBSS prohibitet on this channel [bool]
+	 * NL80211_FREQUENCY_ATTR_RADAR : mandatory radar detection [bool]
+	 * NL80211_FREQUENCY_ATTR_MAX_TX_POWER : max transmission power in 100*dBm [32 bit integer]
+	 *
+	 * NL80211_BITRATE_ATTR_RATE : bitrate in 100 kbps [32 bit integer]
+	 * NL80211_BITRATE_ATTR_2GHZ_SHORTPREAMBLE : short preamble supported [bool]
+ 	 */
+	static struct nla_policy frq_policy[NL80211_FREQUENCY_ATTR_MAX + 1] = {
+		[NL80211_FREQUENCY_ATTR_FREQ] = { .type = NLA_U32 },
+                [NL80211_FREQUENCY_ATTR_DISABLED] = { .type = NLA_FLAG },
+                [NL80211_FREQUENCY_ATTR_PASSIVE_SCAN] = { .type = NLA_FLAG },
+                [NL80211_FREQUENCY_ATTR_NO_IBSS] = { .type = NLA_FLAG },
+                [NL80211_FREQUENCY_ATTR_RADAR] = { .type = NLA_FLAG },
+                [NL80211_FREQUENCY_ATTR_MAX_TX_POWER] = { .type = NLA_U32 },
+        };
+
+	static struct nla_policy btr_policy[NL80211_BITRATE_ATTR_MAX + 1] = {
+		[NL80211_BITRATE_ATTR_RATE] = { .type = NLA_U32 },
+                [NL80211_BITRATE_ATTR_2GHZ_SHORTPREAMBLE] = { .type = NLA_FLAG },
+        };
+
+	/* Set pointer (in message) at payload of netlink header. This message is a
+         * generic netlink message so it will point to generic netlink header.
+	 */
+	struct genlmsghdr *gnlh = nlmsg_data(nlmsg_hdr(msg));
+
+	/* Parse message */
+	nla_parse( msg_attr_buff,		// destination array with maxtype+1 elements
+		   NL80211_ATTR_MAX,		// maximum attribute type to be expected
+		   genlmsg_attrdata(gnlh, 0),	// head of attribute stream ( familly header size =0 )
+		   genlmsg_attrlen(gnlh, 0), 	// length of attribute stream
+		   NULL );			// validation policy			
+	
+	// NL80211_ATTR_WIPHY_BANDS is not optional.
+	// if message lacks this attribute it is useless - skip to next message.
+	if(!msg_attr_buff[NL80211_ATTR_WIPHY_BANDS])
+		return NL_SKIP;
+
+						
+	printf("--------------------------------------------------------\n");
+	printf("Phy ----------------------------------------------------\n");
+	printf("--------------------------------------------------------\n");
+
+	// Device name
+	if (msg_attr_buff[NL80211_ATTR_WIPHY_NAME])
+	{
+               	printf("name: %s\n", nla_get_string(msg_attr_buff[NL80211_ATTR_WIPHY_NAME]));
+		printf("--------------------------------------------------------\n");
+	}
+
+	// Bands
+	nla_for_each_nested(pbnd, msg_attr_buff[NL80211_ATTR_WIPHY_BANDS], rem_bnd) 
+	{                      
+               	printf("	band %d:\n", id_bnd);
+		printf("	------------------------------------------------\n");
+        	id_bnd++;
+
+		/* Parse bands */
+                nla_parse(bnd_attr_buff, NL80211_BAND_ATTR_MAX, nla_data(pbnd),
+                          nla_len(pbnd), NULL);
+		
+		// Bitrates in band
+		printf("		----------------------------------------\n");
+                printf("                bitrates:\n");
+		printf("		----------------------------------------\n");
+
+                nla_for_each_nested(pbtr, bnd_attr_buff[NL80211_BAND_ATTR_RATES], rem_btr)
+                {
+                        /* parse bitrates */
+                        nla_parse(btr_attr_buff, NL80211_BITRATE_ATTR_MAX, nla_data(pbtr),
+                                nla_len(pbtr), btr_policy);
+
+                        // useless - skip
+                        if(!btr_attr_buff[NL80211_BITRATE_ATTR_RATE])
+                                continue;
+
+                        /* bitrate is in [100 kbps] */
+                        printf("                        ");
+                        printf("%2.2f Mbps", 0.1 * nla_get_u32(btr_attr_buff[NL80211_BITRATE_ATTR_RATE]));
+
+                        if(nla_get_u32(btr_attr_buff[NL80211_BITRATE_ATTR_RATE]) < 99)
+                                printf(" ");
+
+                        printf(" ; short preamble: ");
+                        if(btr_attr_buff[NL80211_BITRATE_ATTR_2GHZ_SHORTPREAMBLE])
+                                printf(" OK\n");
+                        else
+                                printf(" -\n");
+
+                }
+		
+		// Frequencies in band
+		printf("		----------------------------------------\n");
+		printf("		frequencies:\n");
+		printf("		----------------------------------------\n");
+		
+		nla_for_each_nested(pfrq, bnd_attr_buff[NL80211_BAND_ATTR_FREQS], rem_frq) 
+		{
+			uint32_t frq;
+			/* Parse frequencies */
+			nla_parse(frq_attr_buff, NL80211_FREQUENCY_ATTR_MAX, nla_data(pfrq), 
+				  nla_len(pfrq), frq_policy);
+			
+			// useless - skip
+			if(!frq_attr_buff[NL80211_FREQUENCY_ATTR_FREQ])
+				continue;
+			
+			frq = nla_get_u32(frq_attr_buff[NL80211_FREQUENCY_ATTR_FREQ]);
+			printf("			%d MHz\n", frq);
+		}
+	
+		// Supported types of interface
+		printf("	------------------------------------------------\n");
+		printf("	avalible interface types:\n");
+		printf("	------------------------------------------------\n");
+		
+		if(msg_attr_buff[NL80211_ATTR_SUPPORTED_IFTYPES])
+		{
+			nla_for_each_nested(pmod, msg_attr_buff[NL80211_ATTR_SUPPORTED_IFTYPES] ,rem_mod)	
+			{
+				printf("		");
+				/* nla_type in nlattr is of type enum nl80211_iftype */
+				printf("%s\n", if_types[pmod->nla_type]);
+			}
+		}	
+	printf("--------------------------------------------------------\n");
+	}
+
+	return NL_SKIP;
+
+}
---
+++ trunk/chnl-switch-sender/scan.c
@@ -0,0 +1,266 @@
+/* 
+ * Author: Marcin Harasimczuk
+ * 
+ * Contains main function ( program flow ).
+ *
+ */
+
+#include <stdio.h>
+#include <netlink/genl/genl.h>
+#include <netlink/genl/family.h>
+#include <netlink/genl/ctrl.h>  
+#include <netlink/msg.h>
+#include <netlink/attr.h>
+
+#include "scan.h"
+#include "cb_handlers.c"
+#include "sysr.c"
+
+/* Utility form Johannes Berg project "iw" */
+#define DIV_ROUND_UP(x, y) (((x) + (y - 1)) / (y))
+
+int parse_hex_mask(char *hexmask, unsigned char **result, size_t *result_len,
+		   unsigned char **mask)
+{
+	size_t len = strlen(hexmask) / 2;
+	unsigned char *result_val;
+	unsigned char *result_mask = NULL;
+
+	int pos = 0;
+
+	*result_len = 0;
+
+	result_val = calloc(len + 2, 1);
+	if (!result_val)
+		goto error;
+	*result = result_val;
+	if (mask) {
+		result_mask = calloc(DIV_ROUND_UP(len, 8) + 2, 1);
+		if (!result_mask)
+			goto error;
+		*mask = result_mask;
+	}
+
+	while (1) {
+		char *cp = strchr(hexmask, ':');
+		if (cp) {
+			*cp = 0;
+			cp++;
+		}
+
+		if (result_mask && (strcmp(hexmask, "-") == 0 ||
+				    strcmp(hexmask, "xx") == 0 ||
+				    strcmp(hexmask, "--") == 0)) {
+			/* skip this byte and leave mask bit unset */
+		} else {
+			int temp, mask_pos;
+			char *end;
+
+			temp = strtoul(hexmask, &end, 16);
+			if (*end)
+				goto error;
+			if (temp < 0 || temp > 255)
+				goto error;
+			result_val[pos] = temp;
+
+			mask_pos = pos / 8;
+			if (result_mask)
+				result_mask[mask_pos] |= 1 << (pos % 8);
+		}
+
+		(*result_len)++;
+		pos++;
+
+		if (!cp)
+			break;
+		hexmask = cp;
+	}
+
+	return 0;
+ error:
+	free(result_val);
+	free(result_mask);
+	return -1;
+}
+
+unsigned char *parse_hex(char *hex, size_t *outlen)
+{
+	unsigned char *result;
+
+	if (parse_hex_mask(hex, &result, outlen, NULL))
+		return NULL;
+	return result;
+}
+
+/* Initialise connection data structure */
+int conn_init( struct conn_data *cd )
+{
+	int error = 0;
+	
+	/*
+	 * INITIALISE NETLINK DATA
+	 */
+
+	// initialise socket
+	cd->nl_sock = nl_handle_alloc();
+	if(!cd->nl_sock)
+	{	
+		fprintf(stderr, "cannot allocate socket\n");
+		return -1;
+	}
+	// connect to generic netlink
+	error = genl_connect(cd->nl_sock);
+	if(error)
+	{
+		fprintf(stderr, "cannot connect to netlink - clean!\n");
+		nl_handle_destroy(cd->nl_sock);
+		return -2;	
+	}
+	// allocate generic netlink cache
+	cd->nl_cache = genl_ctrl_alloc_cache(cd->nl_sock);
+	if(!cd->nl_cache)
+	{
+		fprintf(stderr, "failed to allocate netlink cache! - clean!\n");
+		nl_handle_destroy(cd->nl_sock);
+		return -3;
+	}
+
+	// set netlink family - find if exists
+	cd->nl80211 = genl_ctrl_search_by_name(cd->nl_cache, "nl80211");
+	if(!cd->nl80211)
+	{
+		fprintf(stderr, "nl80211 not present! - clean!\n");
+		nl_cache_free(cd->nl_cache);
+		nl_handle_destroy(cd->nl_sock);
+		return -4;
+	}
+	
+	return 0;
+}
+
+/* Clean connection data structure */
+void conn_clean( struct conn_data *cd)
+{	
+		genl_family_put(cd->nl80211);
+		nl_cache_free(cd->nl_cache);
+		nl_handle_destroy(cd->nl_sock);
+}
+ 
+/* Main program flow */
+int main(int argc, char **argv)
+{
+	// connection data
+	struct conn_data cd;
+	// callback
+	struct nl_cb *cb;
+	struct nl_cb *s_cb;
+	// message
+	struct nl_msg *msg;
+	int error;
+	int devid = 0;
+	// ies
+	size_t ies_size;
+	unsigned char *ies_data;
+	struct nl_msg *ssids = NULL;
+	struct nl_msg *freqs = NULL;
+	
+	// init for ies
+	ssids = nlmsg_alloc();
+	freqs = nlmsg_alloc();
+
+	//@TODO check if allocated!
+	
+
+	/*
+	 * INITIALIZE CONNECTION
+	 */ 
+	error = conn_init(&cd);
+	if(error < 0) 
+		return -1;
+	
+	/*
+   	 * PREPARE MESSAGE
+         */
+	
+	// allocate message
+	msg = nlmsg_alloc();
+	if(!msg)
+	{
+		fprintf(stderr, "failed to allocate netlink message - clean!\n");
+		conn_clean(&cd);
+		return -2;
+	}
+
+	// allocate callbacks
+	cb = nl_cb_alloc(NL_CB_DEFAULT);
+	s_cb = nl_cb_alloc(NL_CB_DEFAULT);
+	if(!cb || !s_cb)
+	{
+		fprintf(stderr, "failed to allocate netlink callbacks - clean!\n");
+		nlmsg_free(msg);
+		conn_clean(&cd);
+		return -3;
+	}
+
+	/*
+         * BUILD MESSAGE
+         */
+	
+	// Trigger scan request
+	genlmsg_put(msg, 0, 0, genl_family_get_id(cd.nl80211), 0, 0, NL80211_CMD_TRIGGER_SCAN, 0);
+	
+	//@TODO: Add attr devid
+	devid = if_nametoindex("wlan0");
+	printf("devid(%d)", devid);
+	NLA_PUT_U32(msg, NL80211_ATTR_IFINDEX, devid);
+	
+	//@TODO: Command handler
+	// freq
+	NLA_PUT_U32(freqs, 1, 2422);
+	// Add ies
+	NLA_PUT(msg, NL80211_ATTR_IE, 64, "123456");
+	//ssids
+	NLA_PUT(ssids, 1, 0, "");
+	nla_put_nested(msg, NL80211_ATTR_SCAN_SSIDS, ssids);
+	nla_put_nested(msg, NL80211_ATTR_SCAN_FREQUENCIES, freqs);
+
+	//Added in new
+	nl_socket_set_cb(cd.nl_sock, s_cb);
+		
+	/*
+	 * SEND MESSAGE
+ 	 */	
+	error = nl_send_auto_complete(cd.nl_sock, msg);
+	if(error < 0)
+	{
+		printf("err!");	
+		fprintf(stderr, "cannot send message! - clean!\n");
+		nl_cb_put(cb);
+		nlmsg_free(msg);
+		conn_clean(&cd);
+		return -4;
+	}
+	/*
+	 * RECIEVE MESSAGE
+	 */
+	/* Callbacks control the receiving of messages (NL_SKIP, NL_STOP).
+	 * Callbacks set error as 0 if there are no more messages to be
+	 * expected.
+	 */
+	error = 1;
+	nl_cb_err(cb, NL_CB_CUSTOM, err_handler, &error);
+        nl_cb_set(cb, NL_CB_FINISH, NL_CB_CUSTOM, fin_handler, &error);
+        nl_cb_set(cb, NL_CB_ACK, NL_CB_CUSTOM, ack_handler, &error);
+	// wait for response
+	while(error > 0)
+		nl_recvmsgs(cd.nl_sock, cb);
+
+	nl_cb_put(cb);
+	nlmsg_free(msg);
+	
+	// cleanup 
+	conn_clean(&cd);
+	
+nla_put_failure:
+	return 0;
+}
